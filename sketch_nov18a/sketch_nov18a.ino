974e9f1290bc51a3b187eb73d4f23a2631c8d50e
notYoosung <98567880+notYoosung@users.noreply.github.com>
2024-11-18 15:51:00 -0600

    refs

--- sketch_nov18a/sketch_nov18a.ino
+++ sketch_nov18a/sketch_nov18a.ino
@@ -11,3 +11,5 @@ void loop() {
   digitalWrite(led, LOW);
   delay(1000);
 }
+// https://lucid.app/lucidchart/fd240305-71fa-484c-a09c-ff9a34840e35/edit?beaconFlowId=265A44DBB6BE8A10&invitationId=inv_3ceb8bef-9edb-4ac3-9b21-026712466c3d&page=0_0#
+// https://docs.arduino.cc/tutorials/uno-r4-wifi/led-matrix/
